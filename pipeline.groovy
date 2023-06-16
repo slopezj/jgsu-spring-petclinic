343a5280fff241e122aaf0463628004b48560e1a
slopezj <slopezj@users.noreply.github.com>
2023-06-16 11:06:49 -0400

    Fixing a test

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,26 +1,16 @@
 pipeline {
     agent any
-
-    //tools {
-        // Install the Maven version configured as "M3" and add it to the path.
-        //maven "M3"
-    //}
-
     stages {
         stage('Checkout') {
             steps {
                 // Get some code from a GitHub repository
-                git url: 'https://github.com/slopezj/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/slopezj/jgsu-spring-petclinic.git', 
+                branch: 'main'
             }
         }
         
         stage('Build') {
-            steps {
-                // Run Maven on a Unix agent.
-                //sh "mvn -Dmaven.test.failure.ignore=true clean package"
-
-                // To run Maven on a Windows agent, use
-                //bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            steps {             
                 bat "mvn clean package"
             }
         
